0ee7f39fcc2f7018944cff1ae5f2f54216ab76e0
Iqbal Ahmed <iqbal.ahmed@digital.cabinet-office.gov.uk>
2021-08-18 18:07:17 +0100

    PP-8263 - Add script to run `pre-commit install` on local repositories
    - Update README.md file with the latest instructions.
    - Add a script that will do the following:
      - `cd` into each local `pay` Git repo.
      - Pull the latest changes from the `master` or `main` branch.
      - Delete the existing Git `pre-commit` hooks
        - Remove `.git/hooks/pre-commit*`
      - Run `pre-commit install`
        - This will set up the the latest `pre-commit` hooks
    - Ran `shellcheck` linter and fixed linting issues.
      - Need to ignore the `glob` check in one occurance as the the file is literally
        called `.git/hooks/pre-commit*`

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # pay-set-up-pre-commit
-Script used to install `pre-commit` on all local `pay` Git repos.  This will make sure that the `pre-commit` Git hooks run whenever you try to commit a file.
+
+Script used to install `pre-commit` on all local `pay-*` Git repos.  E.g. `pay-frontend`, `pay-selfservice`.
+
+This will make sure that the `pre-commit` Git hooks run whenever you try to commit a file.
+
+# Instructions
+
+1. Change directory into the top level directory containing all your `pay` Git repositories.
+2. Run the following script from here:
+```shell
+<location-of-script>/install-pre-commit-on-all-pay-repos.sh
+```
+
+# What the script does
+ 
+- Loop through all directories beginning with `pay-`
+  - Will change directory into each `pay-` Git repository.
+    - `e.g. cd pay-frontend`
+  - Will switch to the `master` or `main` branch.
+  - Will pull the latest changes 
+  - Will delete any existing `pre-commit` hooks.
+    - `rm ./git/hooks/pre-commit*`
+  - Will run `pre-commit install`.
+    - This will install the latest Git `pre-commit` hooks using the `.pre-commit.yaml` config file
+
+# If the script fails
+
+The script tries to provide a useful error message.
+Fix the error and run the script again.
+
+
+
+
---
+++ install-pre-commit-on-all-pay-repos.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+echo "START - installing updated 'pre-commit' hooks on local 'pay' Git repos"
+
+echo -n "This script will delete your existing git hooks. Do you want to continue? (y/n): ";
+read -r
+if [  "${REPLY}" != 'y' ]; then
+  echo "Exiting"
+  exit 1
+fi
+
+errorRunningScript=false
+
+for d in pay-*/ ; do
+    echo "********************"
+    echo "Start - Updating: $d"
+    
+    echo "Changing directory into: $d"
+    
+    if ! cd "$d"; then
+        echo "ERROR - could not change into directory: $d"
+        echo "Skipping to next directory"
+        errorRunningScript=true
+        cd ..
+        continue
+    fi
+    
+    echo "Inside: $d"
+    
+    echo "Checking out 'MASTER' branch..."
+
+    if ! git checkout master; then
+        echo "INFO - could not checkout 'MASTER' branch"
+        echo "Trying to checkout 'MAIN' branch..."
+
+        if ! git checkout main; then
+            echo "ERROR - could not checkout 'MAIN' branch"
+            echo "Delete or stash changes for $d and run the script again"
+            echo "Skipping to next directory"
+            errorRunningScript=true
+            cd ..
+            continue
+        fi
+    fi
+
+    echo "Pulling the latest changes..." 
+
+    if ! git pull; then
+        echo "ERROR - could not pull the latest changes for $d"
+        echo "Please clean up $d and run the script again"
+        echo "Skipping to next directory"
+        errorRunningScript=true
+        cd ..
+        continue
+    fi
+
+    FILE=".git/hooks/pre-commit*"
+
+    # shellcheck disable=SC2086
+    if [ -f $FILE ]; then
+        echo "Existing Git 'pre-commit' hooks exist"
+        echo "-- Deleting existing hooks."
+        rm .git/hooks/pre-commit*
+        echo "-- Deleted."
+    fi
+        
+    echo "Installing new Git hooks"
+    
+    if ! pre-commit install; then 
+        echo "ERROR - installing 'pre-commit' hooks in $d"
+        echo "Skipping to next directory"
+        errorRunningScript=true
+        cd ..
+        continue
+    fi
+
+    cd ..
+
+    echo "End - Updating: $d"
+    echo "********************"
+done
+
+if $errorRunningScript ; then 
+    echo "------------------------------------"
+    echo "ERROR RUNNING THE SCRIPT" 
+    echo "Look through the log and fix issues then run the script again."
+    echo "------------------------------------"
+    exit 1
+fi
+
+echo "------------------------------------"
+echo "FINISH - Script has run succesfully."
+echo "------------------------------------"
